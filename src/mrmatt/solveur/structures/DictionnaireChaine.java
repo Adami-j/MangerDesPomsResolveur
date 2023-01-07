5f01930051026b543cd2025bb4edaa7b0045c41e
juju <julien.adami@etu.iut-tlse3.fr>
2023-01-07 16:57:00 +0100

    implémentation des méthodes (vide pour l'instant)

--- src/mrmatt/solveur/structures/DictionnaireChaine.java
+++ src/mrmatt/solveur/structures/DictionnaireChaine.java
@@ -1,5 +1,19 @@
 package mrmatt.solveur.structures;
 
 public class DictionnaireChaine<C, V> implements Dictionnaire<C, V> {
-	
+
+    @Override
+    public void inserer(C cle, V valeur) {
+
+    }
+
+    @Override
+    public boolean contient(C cle) {
+        return false;
+    }
+
+    @Override
+    public V valeur(C cle) {
+        return null;
+    }
 }
--- src/mrmatt/solveur/structures/ListeTableau.java
+++ src/mrmatt/solveur/structures/ListeTableau.java
@@ -4,4 +4,33 @@ import java.util.Arrays;
 
 public class ListeTableau<E> implements Liste<E> {
 
+    @Override
+    public void ajouter(E element) {
+
+    }
+
+    @Override
+    public boolean estVide() {
+        return false;
+    }
+
+    @Override
+    public int taille() {
+        return 0;
+    }
+
+    @Override
+    public E enlever(int i) {
+        return null;
+    }
+
+    @Override
+    public E element(int i) {
+        return null;
+    }
+
+    @Override
+    public boolean contient(E e) {
+        return false;
+    }
 }
