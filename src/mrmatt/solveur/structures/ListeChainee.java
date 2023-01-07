5e56c03d57934a83c372f6261893b35c8c1d2dfe
juju <julien.adami@etu.iut-tlse3.fr>
2023-01-07 17:05:26 +0100

    ajout implementation méthodes ListeChainee

--- src/mrmatt/solveur/structures/ListeChainee.java
+++ src/mrmatt/solveur/structures/ListeChainee.java
@@ -2,4 +2,33 @@ package mrmatt.solveur.structures;
 
 public class ListeChainee<T> implements Liste<T> {
 
+    @Override
+    public void ajouter(T element) {
+
+    }
+
+    @Override
+    public boolean estVide() {
+        return false;
+    }
+
+    @Override
+    public int taille() {
+        return 0;
+    }
+
+    @Override
+    public T enlever(int i) {
+        return null;
+    }
+
+    @Override
+    public T element(int i) {
+        return null;
+    }
+
+    @Override
+    public boolean contient(T e) {
+        return false;
+    }
 }
