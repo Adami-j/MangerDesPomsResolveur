ba78de7e7e0d1c60a3ecf8164ac32822b2ad18a7
juju <julien.adami@etu.iut-tlse3.fr>
2023-01-08 22:33:55 +0100

    implémentation Noeud (sans jdoc) ajout fichier config contenu dans le dico avec l'état du niveau

---
+++ src/mrmatt/solveur/Configuration.java
@@ -0,0 +1,14 @@
+package mrmatt.solveur;
+
+import mrmatt.sources.Niveau;
+
+public class Configuration {
+
+    private Niveau etatNiveau;
+
+    public Configuration(Niveau etatNiveau) {
+        this.etatNiveau = etatNiveau;
+    }
+
+
+}
--- src/mrmatt/solveur/Noeud.java
+++ src/mrmatt/solveur/Noeud.java
@@ -1,5 +1,88 @@
 package mrmatt.solveur;
 
+
+import mrmatt.solveur.structures.DictionnaireChaine;
+import mrmatt.sources.Commande;
+import mrmatt.sources.Niveau;
+
+
+
 public class Noeud {
+
+    class Fils{
+
+    }
+    private DictionnaireChaine<Configuration,String> dictionnaireChaine;
+    private  Niveau niveauActuel;
+
+    private Noeud filsHaut;
+    private Noeud filsBas;
+    private Noeud filsGauch;
+    private Noeud filsDroit;
+
+    private String commandes;
+
+    public Noeud(DictionnaireChaine<Configuration, String> dictionnaireChaine, String commandes, Niveau niveauActuel) {
+        this.dictionnaireChaine = dictionnaireChaine;
+        this.commandes = commandes;
+        this.niveauActuel = niveauActuel;
+    }
+
+    public Niveau getNiveauActuel() {
+        return niveauActuel;
+    }
+
+    public Noeud getFilsHaut() {
+        return filsHaut;
+    }
+
+    public Noeud getFilsBas() {
+        return filsBas;
+    }
+
+    public Noeud getFilsGauch() {
+        return filsGauch;
+    }
+
+    public Noeud getFilsDroit() {
+        return filsDroit;
+    }
+
+    public void calculerFils(){
+        if(this.getNiveauActuel().deplacementPossible(Commande.HAUT)){
+            Niveau niveauHaut = this.getNiveauActuel().copier();
+            niveauHaut.deplacer(Commande.HAUT);
+            this.filsHaut = new Noeud(this.dictionnaireChaine,this.commandes+"H",niveauHaut);
+        }
+        if(this.getNiveauActuel().deplacementPossible(Commande.BAS)){
+            Niveau niveauBas = this.getNiveauActuel().copier();
+            niveauBas.deplacer(Commande.BAS);
+            this.filsBas = new Noeud(this.dictionnaireChaine,this.commandes+"B",niveauBas);
+        }
+        if(this.getNiveauActuel().deplacementPossible(Commande.GAUCHE)){
+            Niveau niveauGauche = this.getNiveauActuel().copier();
+            niveauGauche.deplacer(Commande.GAUCHE);
+            this.filsGauch = new Noeud(this.dictionnaireChaine,this.commandes+"G",niveauGauche);
+        }
+        if(this.getNiveauActuel().deplacementPossible(Commande.DROITE)){
+            Niveau niveauDroit = this.getNiveauActuel().copier();
+            niveauDroit.deplacer(Commande.DROITE);
+            this.filsDroit = new Noeud(this.dictionnaireChaine,this.commandes+"D",niveauDroit);
+        }
+
+    }
+
+
+    public boolean equals(Object o){
+        if(o==null){
+            return false;
+        }
+        if(o.getClass()!=this.getClass()){
+            return false;
+        }
+        Noeud n = (Noeud) o;
+        return n==this;
+
+    }
 
 }
