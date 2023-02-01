0de1d458fcc6e4f0a967987e376f68f26db961d9
juju <julien.adami@etu.iut-tlse3.fr>
2023-02-02 00:21:42 +0100

    retouches sur les exceptions

--- src/mrmatt/solveur/structures/ListeTableau.java
+++ src/mrmatt/solveur/structures/ListeTableau.java
@@ -7,7 +7,7 @@ public class ListeTableau<E> implements Liste<E> {
 
     private E[] elements;
 
-    private static final int tailleInitiale=10;
+    private static final int TAILLE_INITIALE =10;
     private int nombreElements;
 
     /**
@@ -16,8 +16,8 @@ public class ListeTableau<E> implements Liste<E> {
      * et qui met à zéro le nombre d'éléments dans le tableau
      */
     public ListeTableau() {
-        elements = (E[]) new Object[tailleInitiale];
-        nombreElements = 0;
+        this.elements = (E[]) new Object[ListeTableau.TAILLE_INITIALE];
+        this.nombreElements = 0;
     }
 
     /**
@@ -26,10 +26,7 @@ public class ListeTableau<E> implements Liste<E> {
      */
     @Override
     public void ajouter(E element) throws IllegalArgumentException{
-
-        if(element==null || !element.getClass().equals(element.getClass())){
-            throw new IllegalArgumentException("Element invalide");
-        }
+        this.illegalArgument(element);
         if (this.taille() == this.elements.length) {
             this.elements = Arrays.copyOf(this.elements, this.taille()*2);
         }
@@ -57,13 +54,12 @@ public class ListeTableau<E> implements Liste<E> {
      * décale tous les éléments de la liste ou leur index est sup a i vers la gauche
      */
     @Override
-    public E enlever(int i) {
+    public E enlever(int i) throws IndexOutOfBoundsException{
         this.outOfBound(i);
         E element = this.elements[i];
         this.elements[i] = null;
         for(int j = i; j<this.nombreElements-1;j++){
             this.elements[j]=this.elements[j+1];
-
         }
         this.nombreElements--;
         return element;
@@ -76,7 +72,7 @@ public class ListeTableau<E> implements Liste<E> {
      * retourne l'élément à l'index i dans la liste
      */
     @Override
-    public E element(int i) {
+    public E element(int i) throws IndexOutOfBoundsException{
         this.outOfBound(i);
         return this.elements[i];
     }
@@ -111,31 +107,42 @@ public class ListeTableau<E> implements Liste<E> {
         }
     }
 
+    /**
+     * @author Julien ADAMI
+     * @param element
+     * @throws IllegalArgumentException
+     * méthode qui contrôle si l'élément est null ou si il n'est pas de type E
+     */
+    public void illegalArgument(E element) throws IllegalArgumentException{
+        if(element==null || !element.getClass().equals(element.getClass())){
+            throw new IllegalArgumentException("Element invalide");
+        }
+    }
+
+    /**
+     * @author Julien ADAMI
+     * @param listeTableau
+     * @return true si les deux listes sont égales
+     */
     @Override
     public boolean equals(Object listeTableau) {
 
         if (listeTableau == null || this.getClass() != listeTableau.getClass()) {
             return false;
         }
-
-
         ListeTableau<?> listeTableau1 = (ListeTableau<?>) listeTableau;
 
         if(listeTableau1.nombreElements!=this.nombreElements || (listeTableau1.estVide()!=this.estVide())){
             return false;
         }
-
         int taille = (listeTableau1.nombreElements+this.nombreElements)/2;
-
         for (int i = 0; i < taille; i++) {
 
             if (!this.elements[i].equals(listeTableau1.elements[i])||this.elements[i]==null) {
                 return false;
             }
         }
-
         return true;
-
     }
 
     @Override
@@ -146,6 +153,5 @@ public class ListeTableau<E> implements Liste<E> {
         }
         return res;
     }
-
 
 }
